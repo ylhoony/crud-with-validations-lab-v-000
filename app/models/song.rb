d8c4ef267849c28de2e90cf544878fae12d4c583
Hoon Lee <yl.hoony@gmail.com>
2018-01-30 02:53:43 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,18 +4,18 @@ class Song < ActiveRecord::Base
     scope: [:release_year, :artist_name],
     message: "cannot be repeated by the same artist in the same year"
   }
-  
+
   # validate :is_repeated_by_the_same_artist_in_the_same_year?
   validates :released, inclusion: { in: [true, false]}
   validate :is_release_year_required?
   validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.new.year }, allow_nil: true
   validates :artist_name, presence: true
 
-  def is_repeated_by_the_same_artist_in_the_same_year?
-    if !!Song.find_by(title: title, artist_name: artist_name, release_year: release_year)
-      errors.add(:title, "cannot be repeated by the same artist in the same year")
-    end
-  end
+  # def is_repeated_by_the_same_artist_in_the_same_year?
+  #   if !!Song.find_by(title: title, artist_name: artist_name, release_year: release_year)
+  #     errors.add(:title, "cannot be repeated by the same artist in the same year")
+  #   end
+  # end
 
   def is_release_year_required?
     if released === true && release_year.nil? == true
