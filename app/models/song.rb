aaef1034914a631e51fc51db290ecce513ee0ddb
Hoon Lee <yl.hoony@gmail.com>
2018-01-30 02:53:39 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,11 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validate :is_repeated_by_the_same_artist_in_the_same_year?
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "cannot be repeated by the same artist in the same year"
+  }
+  
+  # validate :is_repeated_by_the_same_artist_in_the_same_year?
   validates :released, inclusion: { in: [true, false]}
   validate :is_release_year_required?
   validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.new.year }, allow_nil: true
